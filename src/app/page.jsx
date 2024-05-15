d1b3ca00673c7535f3e4060396a4fffd32324d93
JopeGAro <josepeddrogr.99@gmail.com>
2024-05-15 17:45:46 -0600

    animacion a homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,30 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 const Homepage = () => {
   return (
-    <div className="flex h-full lg:flex-row flex-col px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-      {/* Imagen Home  */}
-      <div className="h-1/2 relative  lg:h-full lg:w-1/2">
-        <Image src="/hero.png" alt="" fill className=" object-contain" />
+    <motion.div
+      initial={{ opacity: 0, y: -50 }} // Añadido y: -50 para mover el div hacia arriba
+      animate={{ opacity: 1, y: 0 }} // y: 0 para traer el div a su posición original
+      transition={{ duration: 1, ease: "easeOut", delay: 0.4 }}
+      className="flex h-full lg:flex-row flex-col px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48"
+    >
+      {/* Imagen Home */}
+      <div className="h-1/2 relative lg:h-full lg:w-1/2">
+        <Image src="/hero.png" alt="" fill className="object-contain" />
       </div>
 
       {/* Text Home */}
-      <div className="h-1/2 flex flex-col gap-8 items-center justify-center  lg:h-full lg:w-1/2">
+      <div className="h-1/2 flex flex-col gap-8 items-center justify-center lg:h-full lg:w-1/2">
         {/* TITLE */}
-        <h1 className="text-4xl font-bold md:text-7xl lg:text-8xl 2xl:text-9xl ">Jose Pedro Garcia Rojas </h1>
+        <h1 className="text-4xl font-bold md:text-7xl lg:text-8xl 2xl:text-9xl">
+          Jose Pedro Garcia Rojas
+        </h1>
         {/* DES */}
-
-        <p className="md:text-2xl text-xl lg:text-3xl ">
-          Bienvenid@ a mi portafolio digita, tengo 24 años y soy de México.
+        <p className="md:text-2xl text-xl lg:text-3xl">
+          Bienvenid@ a mi portafolio digital, tengo 24 años y soy de México.
           Tengo 2 años de experiencia en el desarrollo de páginas web y sistemas
           de compras utilizando Java y JavaScript. Estudié la carrera de
           Ingeniería en Sistemas Computacionales. Soy una persona competitiva y
@@ -24,11 +33,15 @@ const Homepage = () => {
         </p>
         {/* botton */}
         <div className="flex gap-4 w-full md:text-xl lg:text-2xl">
-          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">Ver mi trabajo</button>
-          <button className="p-4 rounded-lg ring-1 ring-black">Contactame</button>
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            Ver mi trabajo
+          </button>
+          <button className="p-4 rounded-lg ring-1 ring-black">
+            Contactame
+          </button>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/trasitionProvider.jsx
+++ src/components/trasitionProvider.jsx
@@ -1,24 +1,37 @@
 "use client";
 
-import { AnimatePresence, easeOut} from "framer-motion";
+import { AnimatePresence} from "framer-motion";
 import Navbar from "./navbar";
-import { motion } from "framer-motion";
+import { motion, easeOut } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 const TransitionProvider = ({children}) => {
   const pathName = usePathname()
+
   return (
-    <AnimatePresence>
+    <AnimatePresence mode="wait">
+      {console.log(pathName)}
       <div key={pathName} className="w-screen  h-screen bg-gradient-to-b from-blue-100 to-pink-100 ">
       <motion.div
-          className="h-screen w-screen fixed  bg-red-400 rounded-b-[100px] z-40"
+          className="h-screen w-screen fixed  bg-pink-600 rounded-b-[100px] z-40"
           animate={{ height: "0vh" }}
           exit={{ height: "140vh" }}
-          transition={{ duration: 1.5, ease: easeOut }}
+          transition={{ duration: .5, ease: easeOut }}
+
         />
+        {/* <motion.div
+        
+        initial={{opacity: 1}}
+        animate={{ opacity:0}}
+        exit={{ opacity: 1}}
+        transition={{ duration: .8, ease: easeOut }}
+        className=" m-auto top-0  bottom-0 right-0 left-0 text-white text-8xl  cursor-default z-50  w-fit h-fit fixed ">
+          
+        </motion.div> */}
+
       <motion.div
-          className="h-screen w-screen fixed  bottom-0 bg-blue-500 rounded-t-[100px] z-50"
-          animate={{ height: "0vh", transition: {delay: 1} }}
+          className="h-screen w-screen fixed  bottom-0 bg-blue-600 rounded-t-[100px] z-50"
+          animate={{ height: "0vh", transition: {delay: .5} }}
           initial={{ height: "140vh" }}
           
         />
@@ -26,7 +39,8 @@ const TransitionProvider = ({children}) => {
           <div className="h-24">
             <Navbar />
           </div>
-          <div className="h-[calc(100vh-6rem)] ">{children}</div>
+          <div className="h-[calc(100vh-6rem)] ">{
+          children}</div>
         </div>
     </AnimatePresence>
   );
